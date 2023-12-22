1e1135007efcb391bfa28414beabd48d66d81236
Paulo Costa <paulotcj@gmail.com>
2023-12-22 11:56:26 -0500

    .

--- working/matrix.py
+++ working/matrix.py
@@ -6,8 +6,12 @@ class Symbol:
     def __init__(self, x, y):
         self.x ,self.y = y = x, y
         self.value = choice(green_katakana)
+        # self.interval = randrange(5, 30)
 
     def draw(self):
+        frames = pg.time.get_ticks()
+        # if not frames % self.interval:
+        #     self.value = choice(green_katakana) #choice -> Return a random element from the non-empty sequence seq
         surface.blit(self.value, (self.x, self.y)) #blit modifies the destination surface by drawing at the specified coordinates.
 
 os.environ['SDL_VIDEO_CENTERED'] = '1'
@@ -26,8 +30,11 @@ font = pg.font.Font('MS Mincho.ttf', FONT_SIZE) #"MS Mincho is a Japanese font t
 font.bold = True
 green_katakana = [  font.render(char, True, pg.Color('green'))  for char in katakana ] # probably a C library, the first arg is text, the second is antialiasing, the third is color
 
+symbol = Symbol(0,0)
 while True:
     surface.fill(pg.Color('black'))
+
+    symbol.draw()
 
     [exit() for i in pg.event.get() if i.type == pg.QUIT]
     pg.display.flip() # make all changes made to the screen Surface visible by flipping the offscreen buffer with the onscreen buffer
