296355dd1abdbb976811f4b07abd46e2b042cafd
Paulo Costa <paulotcj@gmail.com>
2023-12-22 14:22:11 -0500

    .

--- working/matrix.py
+++ working/matrix.py
@@ -6,13 +6,13 @@ class Symbol:
     def __init__(self, x, y, speed):
         self.x ,self.y = y = x, y
         self.speed = speed
-        self.value = choice(green_katakana)
+        self.value = choice( green_katakana )
         self.interval = randrange(5, 30)
 
-    def draw(self):
+    def draw(self, color):
         frames = pg.time.get_ticks()
         if not frames % self.interval:
-            self.value = choice(green_katakana) #choice -> Return a random element from the non-empty sequence seq
+            self.value = choice( green_katakana if color == 'green' else lightgreen_katakana ) #choice -> Return a random element from the non-empty sequence seq
         self.y = self.y + self.speed if self.y < HEIGHT else -FONT_SIZE
         surface.blit(self.value, (self.x, self.y)) #blit modifies the destination surface by drawing at the specified coordinates.
 
@@ -23,14 +23,20 @@ class SymbolColumn:
         self.symbols = [ Symbol(x, i, self.speed) for i in range(y, y - FONT_SIZE * self.column_height, - FONT_SIZE) ]
 
     def draw(self):
-        [ symbol.draw() for symbol in self.symbols ]
+        # if i == 0 then draw light green
+        [  symbol.draw('green') if i else symbol.draw('lightgreen')   for i, symbol in enumerate(self.symbols) ]
 
 
 os.environ['SDL_VIDEO_CENTERED'] = '1'
 RES = WIDTH, HEIGHT = 1600, 900 #tuple (1600,900)
 FONT_SIZE = 40
+alpha_value = 0
+
 pg.init()
-surface = pg.display.set_mode(size= RES)
+screen = pg.display.set_mode(RES)
+surface = pg.Surface(RES)
+surface.set_alpha(alpha_value) # set the alpha value for the whole surface
+
 clock = pg.time.Clock()
 
 #int('0x30a0', 16) - > convert the hexadecimal '0x30a0' (base 16) to int = 12448
@@ -40,14 +46,21 @@ katakana = [  chr( 12448 + i )  for i in range(96) ]
 
 font = pg.font.Font('MS Mincho.ttf', FONT_SIZE) #"MS Mincho is a Japanese font that features serifs at the end of its strokes"
 font.bold = True
-green_katakana = [  font.render(char, True, pg.Color('green'))  for char in katakana ] # probably a C library, the first arg is text, the second is antialiasing, the third is color
+#                                            ( R           G          B )  
+green_katakana = [  font.render(char, True,  ( 0, randrange(100,200), 0 )  )  for char in katakana ] # probably a C library, the first arg is text, the second is antialiasing, the third is color
+lightgreen_katakana = [  font.render(char, True, pg.Color('lightgreen'))  for char in katakana ]
 
 # symbol = Symbol(WIDTH//2 - FONT_SIZE//2, HEIGHT//2 - FONT_SIZE//2, speed=5)
-symbol_columns = [ SymbolColumn(x,0) for x in range(0, WIDTH , FONT_SIZE) ]
+symbol_columns = [ SymbolColumn(x,randrange(-HEIGHT, 0)) for x in range(0, WIDTH , FONT_SIZE) ]
 while True:
+    screen.blit(surface, (0,0))
     surface.fill(pg.Color('black'))
 
     [ symbol_column.draw() for symbol_column in symbol_columns ]
+
+    if not pg.time.get_ticks() % 20 and alpha_value < 170:
+        alpha_value += 5
+        surface.set_alpha(alpha_value)
 
     [exit() for i in pg.event.get() if i.type == pg.QUIT]
     pg.display.flip() # make all changes made to the screen Surface visible by flipping the offscreen buffer with the onscreen buffer
