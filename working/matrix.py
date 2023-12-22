7ce1b45dc9566dc7ecda4d4980a8e39a676740cf
Paulo Costa <paulotcj@gmail.com>
2023-12-22 16:26:47 -0500

    .

--- working/matrix.py
+++ working/matrix.py
@@ -14,13 +14,16 @@ class SpecialChar: #Japanese Katakana
         self.screen_resolution = screen_resolution
     #------------------------------------------------------------
     #------------------------------------------------------------
-    def draw(self, color):
+    def draw(self, color, surface):
         frames = self.pygame.time.get_ticks()
         font_size = self.chars['font_size']
         screen_width , screen_height = self.screen_resolution
         
-        if not frames % self.interval:
-            self.value = choice( self.chars['green'] if color == 'green' else self.chars['lightgreen'] ) #choice -> Return a random element from the non-empty sequence seq
+        if (frames % self.interval) == 0:
+            if color == 'green':
+                self.value = choice( self.chars['green'] )
+            else:
+                self.value = choice( self.chars['lightgreen'] )
         
         self.y = self.y + self.speed if self.y < screen_height else (-font_size)
         surface.blit(self.value, (self.x, self.y)) #blit modifies the destination surface by drawing at the specified coordinates.
@@ -33,11 +36,21 @@ class CharColumn:
         self.column_height = randrange(8,18)
         self.speed = randrange(2,6)
         font_size = p_chars['font_size']
-        self.symbols = [ SpecialChar(x, i, self.speed, chars=p_chars , pygame=p_pygame , screen_resolution=screen_resolution) for i in range(y, y - font_size * self.column_height, - font_size) ]
+
+        self.symbols = []
+        for i in range(y, y - font_size * self.column_height, - font_size):
+            temp = SpecialChar(x, i, self.speed, chars=p_chars , pygame=p_pygame , screen_resolution=screen_resolution) 
+            self.symbols.append(temp)
+
     #------------------------------------------------------------
     #------------------------------------------------------------
-    def draw(self):
-        [  i_char.draw('lightgreen') if i == 0 else  i_char.draw('green')  for i, i_char in enumerate(self.symbols) ]
+    def draw(self, surface):
+
+        for i , i_char in enumerate(self.symbols):
+            if i == 0 : #first char in the column
+                i_char.draw('lightgreen',surface)
+            else:
+                i_char.draw('green',surface)
     #------------------------------------------------------------
 #------------------------------------------------------------
 
@@ -71,7 +84,7 @@ def handle_events(pygame):
 def intro_fade(pygame, p_surface, p_alpha_value):
     if not pygame.time.get_ticks() % 20 and p_alpha_value < 170:
         p_alpha_value += 5
-        p_surface.set_alpha(alpha_value)
+        p_surface.set_alpha(p_alpha_value)
     
     return p_surface, p_alpha_value
 #------------------------------------------------------------            
@@ -84,37 +97,37 @@ def create_char_column(screen_resolution, chars_dict, pygame):
 #------------------------------------------------------------            
 
 #------------------------------------------------------------
-# def matrix():
-os.environ['SDL_VIDEO_CENTERED'] = '1'
-SCREEN_RESOLUTION = SCREEN_WIDTH, SCREEN_HEIGHT = 1600, 900 #tuple (1600,900)
-FONT_SIZE = 40
-alpha_value = 0
+def matrix():
+    os.environ['SDL_VIDEO_CENTERED'] = '1'
+    SCREEN_RESOLUTION = SCREEN_WIDTH, SCREEN_HEIGHT = 1600, 900 #tuple (1600,900)
+    FONT_SIZE = 40
+    alpha_value = 0
 
-pg.init()
-#----------------
-screen = pg.display.set_mode(SCREEN_RESOLUTION)
-surface = pg.Surface(SCREEN_RESOLUTION)
-surface.set_alpha(alpha_value) # set the alpha value for the whole surface
-#----------------
-clock = pg.time.Clock()
-#----------------
-chars_dict = create_chars(font_name='MS Mincho.ttf', font_size=FONT_SIZE)
-#----------------
+    pg.init()
+    #----------------
+    screen = pg.display.set_mode(SCREEN_RESOLUTION)
+    surface = pg.Surface(SCREEN_RESOLUTION)
+    surface.set_alpha(alpha_value) # set the alpha value for the whole surface
+    #----------------
+    clock = pg.time.Clock()
+    #----------------
+    chars_dict = create_chars(font_name='MS Mincho.ttf', font_size=FONT_SIZE)
+    #----------------
 
-# symbol_columns = [ CharColumn(x,randrange(-SCREEN_HEIGHT, 0), p_chars=chars_dict, p_pygame=pg) for x in range(0, SCREEN_WIDTH , FONT_SIZE) ]
-symbol_columns = create_char_column(screen_resolution=SCREEN_RESOLUTION, chars_dict = chars_dict, pygame = pg)
-while True:
-    screen.blit(surface, (0,0))
-    surface.fill(pg.Color('black'))
+    # symbol_columns = [ CharColumn(x,randrange(-SCREEN_HEIGHT, 0), p_chars=chars_dict, p_pygame=pg) for x in range(0, SCREEN_WIDTH , FONT_SIZE) ]
+    symbol_columns = create_char_column(screen_resolution=SCREEN_RESOLUTION, chars_dict = chars_dict, pygame = pg)
+    while True:
+        screen.blit(surface, (0,0))
+        surface.fill(pg.Color('black'))
 
-    [ symbol_column.draw() for symbol_column in symbol_columns ]
+        [ symbol_column.draw(surface=surface) for symbol_column in symbol_columns ]
 
-    surface, alpha_value = intro_fade(pygame=pg, p_surface=surface, p_alpha_value=alpha_value)
+        surface, alpha_value = intro_fade(pygame=pg, p_surface=surface, p_alpha_value=alpha_value)
 
-    handle_events(pygame=pg)
+        handle_events(pygame=pg) #keyboard keys, mouse, etc
 
-    pg.display.flip() # make all changes made to the screen Surface visible by flipping the offscreen buffer with the onscreen buffer
+        pg.display.flip() # make all changes made to the screen Surface visible by flipping the offscreen buffer with the onscreen buffer
 
-    clock.tick(60) # control the framerate to 60 FPS
+        clock.tick(60) # control the framerate to 60 FPS
 #------------------------------------------------------------
-# matrix()
+matrix()
