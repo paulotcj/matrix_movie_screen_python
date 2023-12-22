ff9baf6685fd2bf835e1f39d5b931bd4a6c25eb8
Paulo Costa <paulotcj@gmail.com>
2023-12-22 17:47:25 -0500

    .

--- working/matrix.py
+++ working/matrix.py
@@ -4,30 +4,29 @@ from random import choice, randrange
 #------------------------------------------------------------
 class SpecialChar: #Japanese Katakana
     #------------------------------------------------------------
-    def __init__(self, x, y, speed, chars, pygame, screen_resolution):
-        self.x ,self.y = y = x, y
-        self.speed = speed
-        self.value = choice( chars['green'] )
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.value = None
         self.interval = randrange(5, 30)
-        self.pygame = pygame
-        self.chars = chars
-        self.screen_resolution = screen_resolution
     #------------------------------------------------------------
     #------------------------------------------------------------
-    def draw(self, color, surface):
-        frames = self.pygame.time.get_ticks()
-        font_size = self.chars['font_size']
-        screen_width , screen_height = self.screen_resolution
+    def draw(self, color, surface, speed, chars, pygame, screen_resolution):
+        frames = pygame.time.get_ticks()
+        font_size = chars['font_size']
+        screen_width , screen_height = screen_resolution
         
-        if (frames % self.interval) == 0: #this makes the chars flick given a certain interval
+        #-------
+        if (frames % self.interval) == 0 or self.value == None: #this makes the chars flick given a certain interval
             #now considering we are flicking the chars, we need to identify if this is the first char of the column, the first char is always light green
             if color == 'green':
-                self.value = choice( self.chars['green'] )
+                self.value = choice( chars['green'] )
             else:
-                self.value = choice( self.chars['lightgreen'] )
+                self.value = choice( chars['lightgreen'] )
+        #-------
 
         if self.y < screen_height:
-            self.y = self.y + self.speed
+            self.y = self.y + speed
         else:
             self.y = -font_size
 
@@ -41,10 +40,14 @@ class CharColumn:
         self.column_height = randrange(8,18)
         self.speed = randrange(2,6)
         font_size = p_chars['font_size']
+        self.chars = p_chars
+        self.pygame = p_pygame
+        self.screen_resolution = screen_resolution
 
         self.symbols = []
-        for i in range(y, y - font_size * self.column_height, - font_size):
-            temp = SpecialChar(x, i, self.speed, chars=p_chars , pygame=p_pygame , screen_resolution=screen_resolution) 
+        for_to_limit = y - font_size * self.column_height
+        for i in range(y, for_to_limit, -font_size ):
+            temp = SpecialChar(x, i) 
             self.symbols.append(temp)
 
     #------------------------------------------------------------
@@ -53,9 +56,9 @@ class CharColumn:
 
         for i , i_char in enumerate(self.symbols):
             if i == 0 : #first char in the column
-                i_char.draw('lightgreen',surface)
+                i_char.draw('lightgreen',surface, self.speed, chars = self.chars, pygame = self.pygame, screen_resolution=self.screen_resolution)
             else:
-                i_char.draw('green',surface)
+                i_char.draw('green',surface, self.speed, chars = self.chars, pygame = self.pygame, screen_resolution=self.screen_resolution)
     #------------------------------------------------------------
 #------------------------------------------------------------
 
@@ -71,9 +74,9 @@ def create_chars(font_name, font_size):
     font.bold = True
 
 
-    #                                            ( R           G          B )  every char will have a different tint of green
-    green_katakana = [  font.render(char, True,  ( 0, randrange(100,200), 0 )  )  for char in katakana ] # probably a C library, the first arg is text, the second is antialiasing, the third is color
-    lightgreen_katakana = [  font.render(char, True, pg.Color('lightgreen'))  for char in katakana ]
+    #                                                 ( R           G          B )  every char will have a different tint of green
+    green_katakana =      [  font.render(char, True,  ( 0, randrange(100,200), 0 )  )  for char in katakana ] # probably a C library, the first arg is text, the second is antialiasing, the third is color
+    lightgreen_katakana = [  font.render(char, True, pg.Color('lightgreen')         )  for char in katakana ]
     chars_dict = {'green' : green_katakana, 'lightgreen' : lightgreen_katakana, 'plain' : katakana , 'font_size' : font_size, 'font_name' : font_name}
 
 
@@ -97,7 +100,15 @@ def intro_fade(pygame, p_surface, p_alpha_value):
 def create_char_column(screen_resolution, chars_dict, pygame):
     screen_width , screen_height = screen_resolution
     font_size = chars_dict['font_size']
-    symbol_columns = [ CharColumn(x,randrange(-screen_height, 0), p_chars=chars_dict, p_pygame=pygame ,screen_resolution=screen_resolution) for x in range(0, screen_width , font_size) ]
+
+    #-----
+    symbol_columns = []
+    for x in range(0, screen_width , font_size):
+        y = randrange(-screen_height, 0)
+        temp = CharColumn(x,y, p_chars=chars_dict, p_pygame=pygame ,screen_resolution=screen_resolution) 
+        symbol_columns.append(temp)
+    #-----
+
     return symbol_columns
 #------------------------------------------------------------            
 
@@ -119,7 +130,6 @@ def matrix():
     chars_dict = create_chars(font_name='MS Mincho.ttf', font_size=FONT_SIZE)
     #----------------
 
-    # symbol_columns = [ CharColumn(x,randrange(-SCREEN_HEIGHT, 0), p_chars=chars_dict, p_pygame=pg) for x in range(0, SCREEN_WIDTH , FONT_SIZE) ]
     symbol_columns = create_char_column(screen_resolution=SCREEN_RESOLUTION, chars_dict = chars_dict, pygame = pg)
     while True:
         screen.blit(surface, (0,0))
