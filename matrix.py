5bea7041900f9b675fdb9dce113095cb12a96d26
Paulo Costa <paulotcj@gmail.com>
2023-12-22 11:03:00 -0500

    .

--- matrix.py
+++ matrix.py
@@ -2,6 +2,14 @@ import os
 import pygame as pg
 from random import choice, randrange
 
+class Symbol:
+    def __init__(self, x, y):
+        self.x ,self.y = y = x, y
+        self.value = choice(green_katakana)
+
+    def draw(self):
+        surface.blit(self.value, (self.x, self.y)) #blit modifies the destination surface by drawing at the specified coordinates.
+
 os.environ['SDL_VIDEO_CENTERED'] = '1'
 RES = WIDTH, HEIGHT = 1600, 900 #tuple (1600,900)
 FONT_SIZE = 40
@@ -13,6 +21,10 @@ clock = pg.time.Clock()
 # the original reference to katakana chars is: u+30AX which means '0x30a0' in hex, but for simplicity we use the int value
 # katakana = [  chr( int('0x30a0', 16) + i )  for i in range(96) ]
 katakana = [  chr( 12448 + i )  for i in range(96) ]
+
+font = pg.font.Font('MS Mincho.ttf', FONT_SIZE) #"MS Mincho is a Japanese font that features serifs at the end of its strokes"
+font.bold = True
+green_katakana = [  font.render(char, True, pg.Color('green'))  for char in katakana ] # probably a C library, the first arg is text, the second is antialiasing, the third is color
 
 while True:
     surface.fill(pg.Color('black'))
