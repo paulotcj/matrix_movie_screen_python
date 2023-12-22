5298e1c2d6a80814c203bc8dcdc0b78871cc59c8
Paulo Costa <paulotcj@gmail.com>
2023-12-22 14:29:38 -0500

    .

--- working/matrix.py
+++ working/matrix.py
@@ -1,32 +1,41 @@
 import os
 import pygame as pg
 from random import choice, randrange
-
+#------------------------------------------------------------
 class Symbol:
+    #------------------------------------------------------------
     def __init__(self, x, y, speed):
         self.x ,self.y = y = x, y
         self.speed = speed
         self.value = choice( green_katakana )
         self.interval = randrange(5, 30)
-
+    #------------------------------------------------------------
+    #------------------------------------------------------------
     def draw(self, color):
         frames = pg.time.get_ticks()
         if not frames % self.interval:
             self.value = choice( green_katakana if color == 'green' else lightgreen_katakana ) #choice -> Return a random element from the non-empty sequence seq
         self.y = self.y + self.speed if self.y < HEIGHT else -FONT_SIZE
         surface.blit(self.value, (self.x, self.y)) #blit modifies the destination surface by drawing at the specified coordinates.
-
+    #------------------------------------------------------------
+#------------------------------------------------------------
+#------------------------------------------------------------
 class SymbolColumn:
+    #------------------------------------------------------------
     def __init__(self, x, y):
         self.column_height = randrange(8,18)
         self.speed = randrange(2,6)
         self.symbols = [ Symbol(x, i, self.speed) for i in range(y, y - FONT_SIZE * self.column_height, - FONT_SIZE) ]
-
+    #------------------------------------------------------------
+    #------------------------------------------------------------
     def draw(self):
         # if i == 0 then draw light green
         [  symbol.draw('green') if i else symbol.draw('lightgreen')   for i, symbol in enumerate(self.symbols) ]
+    #------------------------------------------------------------
+#------------------------------------------------------------
 
-
+#------------------------------------------------------------
+# def matrix():
 os.environ['SDL_VIDEO_CENTERED'] = '1'
 RES = WIDTH, HEIGHT = 1600, 900 #tuple (1600,900)
 FONT_SIZE = 40
@@ -66,3 +75,5 @@ while True:
     pg.display.flip() # make all changes made to the screen Surface visible by flipping the offscreen buffer with the onscreen buffer
 
     clock.tick(60) # control the framerate to 60 FPS
+#------------------------------------------------------------
+# matrix()
